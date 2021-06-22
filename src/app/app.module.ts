7539035307344c1a5c6939187ab8868d509cc6dc
said-oumakhlouf <oumakhlouf.said@gmail.com>
2021-06-22 02:08:39 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HotelListComponent } from './components/hotel-list/hotel-list.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
--- src/app/components/hotel-list/hotel-list.component.ts
+++ src/app/components/hotel-list/hotel-list.component.ts
@@ -7,11 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelListComponent implements OnInit {
 
-  title = 'hotel-app';
+  title = 'Liste des hotels';
   
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public hotels: any[] = [
+    {
+          "hotelId": 1,
+          "hotelName": "Buea sweet life",
+          "description": "Belle vue au bord de la mer",
+          "price": 230.5,
+          "imageUrl": "assets/img/hotel-mer.jpg",
+          "rating": 3.5
+    },
+    {
+          "hotelId": 2,
+          "hotelName": "Marrakech",
+          "description": "Profitez de la vue sur les montagnes",
+          "price": 145.5,
+          "imageUrl": "assets/img/marrakech.jpg",
+          "rating": 5
+    },
+    {
+          "hotelId": 3,
+          "hotelName": "Abudja new look palace",
+          "description": "Séjour complet avec service de voitures",
+          "price": 120.12,
+          "imageUrl": "assets/img/abudja.jpg",
+          "rating": 2.5
+    },
+    {
+          "hotelId": 4,
+          "hotelName": "Mexico",
+          "description": "Magnifique cadre pour votre séjour",
+          "price": 135.12,
+          "imageUrl": "assets/img/mexique.jpg",
+          "rating": 5
+    }
+  ];
+
+  public showBadge!: boolean;
+
+  public hotelFilter = 'mot';
+
+  public toggleIsNewBadge(): void {
+    this.showBadge = !this.showBadge;
+  };
 
 }
